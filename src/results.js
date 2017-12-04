cb783d04acf3c5e42e855340ff764943f2002f99
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-12-04 05:30:13 -0600

    day2 part 1

--- src/results.js
+++ src/results.js
@@ -1,7 +1,8 @@
 import { day1 } from './day-1';
+import { day2 } from './day-2';
 
 function result(fn) {
   console.log(`${fn.name} solution is ${fn()}`); // eslint-disable-line no-console
 }
 
-[day1].forEach(result);
+[day1, day2].forEach(result);
