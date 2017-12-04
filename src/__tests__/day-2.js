ce5d4cd179200d366c2133766686709f513e312d
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-12-04 06:58:42 -0600

    update day2 to be an evenly divisible solver

--- src/__tests__/day-2.js
+++ src/__tests__/day-2.js
@@ -2,9 +2,9 @@ import solver from '../day-2';
 
 describe('Day 2', () => {
   it('can solve the example', () => {
-    const spreadsheet = `5 1 9 5
-    7 5 3
-    2 4 6 8`;
-    expect(solver(spreadsheet)).toEqual(18);
+    const spreadsheet = `5 9 2 8
+    9 4 7 3
+    3 8 6 5`;
+    expect(solver(spreadsheet)).toEqual(9);
   });
 });
--- src/day-2.js
+++ src/day-2.js
@@ -8,12 +8,27 @@ function maxMin(arr) {
     { max: first, min: first }
   );
 }
+
+function evenlyDivisible(arr) {
+  return arr.map(x => parseInt(x, 10)).reduce((evenly, num, idx, allNumbers) => {
+    if (evenly) {
+      return evenly;
+    }
+    // eslint-disable-next-line no-plusplus
+    for (let i = idx + 1; i < allNumbers.length; i++) {
+      const curr = allNumbers[i];
+      if (num !== curr && Math.max(num, curr) % Math.min(num, curr) === 0) {
+        return Math.max(num, curr) / Math.min(num, curr);
+      }
+    }
+
+    return null;
+  }, null);
+}
+
 export default function solver(spreadsheet) {
   const rows = spreadsheet.split('\n').map(x => x.trim().split(/\s+/));
-  return rows.reduce((acc, row) => {
-    const rowMaxMin = maxMin(row);
-    return acc + (rowMaxMin.max - rowMaxMin.min);
-  }, 0);
+  return rows.reduce((acc, row) => acc + evenlyDivisible(row), 0);
 }
 
 export function day2() {
